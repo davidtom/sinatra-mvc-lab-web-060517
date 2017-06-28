d77e796295ec175d73fe62fe8cc4afbd33933ceb
Diljeet <jeetds1994@gmail.com>
2017-06-28 10:37:19 -0400

    ompletecay ablay

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  set :views, 'views'
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @phrase = params[:user_phrase]
+    pl = PigLatinizer.new
+    @pig_latin = pl.to_pig_latin(@phrase)
+    erb :piglatinize
+  end
 end
